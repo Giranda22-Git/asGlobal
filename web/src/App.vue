fe8b177e3ebe8cc3ddd4a706b665b01efcd7c5bf
Giranda22-Git <aloxaeee@bk.ru>
2020-07-01 13:45:40 +0600

    project init

---
+++ web/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <router-link to="/">Home</router-link>
+      <router-link to="/catalog">About</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>

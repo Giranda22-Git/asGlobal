689668a474b1a11a74397782518463cb91e2f98b
RerrorHandler <61464119+RerrorHandler@users.noreply.github.com>
2020-07-02 15:58:40 +0600

    Footer added + partner
    add footer and parner template
    eldar lox

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,13 +3,18 @@
       <router-link to="/">Home</router-link>
       <router-link to="/catalog">About</router-link>
     <router-view/>
+    <theFooter/>
   </div>
 </template>
 
 <script>
+import theFooter from '@/components/theFooter.vue'
 export default {
   data: {
     
+  },
+  components: {
+    theFooter
   }
 }
 </script>

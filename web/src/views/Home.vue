7413e17472ff5d08cbef8786eb4a16cdddb060df
Giranda22-Git <aloxaeee@bk.ru>
2020-07-10 20:06:49 +0600

    bux fix

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -34,19 +34,19 @@ export default {
         { id: 7, name: 'img4', img: 'carousel4.jpg' }
       ],
       catalogData: [
-        { id: 0, name: 'productName0', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 1, name: 'productName1', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 2, name: 'productName2', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 3, name: 'productName3', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 4, name: 'productName4', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 5, name: 'productName5', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 6, name: 'productName6', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 7, name: 'productName7', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 8, name: 'productName8', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 9, name: 'productName9', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 10, name: 'productName10', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 11, name: 'productName11', img: 'productimage.jpg', description: 'Catalog Name' },
-        { id: 12, name: 'productName12', img: 'productimage.jpg', description: 'Catalog Name' }
+        { id: 0, name: 'Catalog Name1', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 1, name: 'Catalog Name2', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 2, name: 'Catalog Name3', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 3, name: 'Catalog Name4', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 4, name: 'Catalog Name5', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 5, name: 'Catalog Name6', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 6, name: 'Catalog Name7', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 7, name: 'Catalog Name8', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 8, name: 'Catalog Name9', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 9, name: 'Catalog Name10', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 10, name: 'Catalog Name11', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 11, name: 'Catalog Name12', img: 'productimage.jpg', description: 'Catalog Name' },
+        { id: 12, name: 'Catalog Name13', img: 'productimage.jpg', description: 'Catalog Name' }
       ]
     }
   },
